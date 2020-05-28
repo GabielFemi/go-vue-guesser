f7847f53b2aa453641d14d4a4e48f35b327b4e80
GabielFemi <gabrielfemi799@gmail.com>
2020-05-28 21:43:11 +0200

    add prop for placeholder
    Took 8 minutes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,15 @@
 const guessComponent = {
     template:
-    `<input name="guess" class="input is-small" type="text" placeholder="Guess a number" />`
+    `<input name="guess" class="input is-64x64" type="text" :placeholder = "placeholder"/>`,
+    props: ['placeholder']
 }
 new Vue({
     el: '#app',
-
+    data: {
+        notes: [],
+        guesses: [],
+        placeholder: 'Guess what I\'m thinking'
+    },
     computed: {
 
     },
